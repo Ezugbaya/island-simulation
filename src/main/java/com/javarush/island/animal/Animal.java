b3a44b2129486ed4c2dbc6ed320f05f284f39398
Ilya Ezugbaya <ilya33ruus@gmail.com>
2026-03-01 16:48:39 +0300

    add base animals class

--- src/main/java/com/javarush/island/animal/Animal.java
+++ src/main/java/com/javarush/island/animal/Animal.java
@@ -1,5 +1,15 @@
 package com.javarush.island.animal;
 
-public class Animal {
-    protected  double weight;
+import com.javarush.island.model.Location;
+import lombok.Getter;
+
+@Getter
+public abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat(Location location);
 }
