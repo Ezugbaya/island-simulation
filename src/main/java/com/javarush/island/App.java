ee99f868a18621c0e112e4787bd5c1c1a12020b9
Ilya Ezugbaya <ilya33ruus@gmail.com>
2026-03-01 17:22:58 +0300

    project health check

--- src/main/java/com/javarush/island/App.java
+++ src/main/java/com/javarush/island/App.java
@@ -1,13 +1,18 @@
 package com.javarush.island;
 
+import com.javarush.island.config.SimulationConfig;
+import com.javarush.island.model.Island;
 import lombok.extern.slf4j.Slf4j;
 
-// Lombok создаёт logger автоматически
+
 @Slf4j
 public class App {
 
     public static void main(String[] args) {
 
-        log.info("Проверка");
+        // создаем остров
+        Island island = new Island(SimulationConfig.ROWS,SimulationConfig.COLS);
+
+        log.info("Создан остров: {} x {}",island.getRows(),island.getCols());
     }
 }
